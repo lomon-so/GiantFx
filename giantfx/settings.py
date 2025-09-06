be3f6a0d8e8da86d1069ee5a8ffb454c4409b68e
lomon-so <solomonchima91@gmail.com>
2025-09-06 14:36:57 +0100

    Add allowd host for deployment

--- giantfx/settings.py
+++ giantfx/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'giantfx-ptu3.onrender.com').split(',')
+
 
 
 # Application definition
